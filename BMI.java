62c3a0ce6eed0e30ed90faa9f583cbedead4d807
ChiJui-c110181119 <93572490+ChiJui-c110181119@users.noreply.github.com>
2021-11-09 16:31:31 +0800

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,7 @@
+public class BMI {
+    public static void main(String[] args) {
+        int a =175;
+        int b =80;
+        System.out.println(b/((a/100)^2));
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,6 @@
+public class Variable {
+    public static void main(String[] args){
+        String myname = "Chijui Huang";
+        System.out.println("Hello " + myname);
+    }
+}
