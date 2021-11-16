bc113c6835324153f663e08ca210b83a1263dd05
ChiJui-c110181119 <93572490+ChiJui-c110181119@users.noreply.github.com>
2021-11-16 16:30:12 +0800

    Add files via upload

---
+++ If_else.java
@@ -0,0 +1,10 @@
+public class If_else {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 21 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+//%->餘數
---
+++ comment.java
@@ -0,0 +1,8 @@
+public class comment {
+    public static void main(String[] args){
+        //This is comment
+        //這是註解
+        //comment
+        //註解
+    }
+}
